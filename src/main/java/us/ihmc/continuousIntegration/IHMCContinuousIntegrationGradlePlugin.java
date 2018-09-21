756fdf3d2930e91d9ec2a35eac621d49873516b2
Duncan Calvert <dcalvert@ihmc.us>
2018-09-21 12:21:04 -0500

    Start on testing parallel execution.

--- src/main/java/us/ihmc/continuousIntegration/IHMCContinuousIntegrationGradlePlugin.java
+++ src/main/java/us/ihmc/continuousIntegration/IHMCContinuousIntegrationGradlePlugin.java
@@ -1,16 +1,18 @@
 package us.ihmc.continuousIntegration;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.codehaus.groovy.runtime.MethodClosure;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-
+import org.gradle.api.UnknownDomainObjectException;
+import org.gradle.api.tasks.testing.Test;
+import org.gradle.api.tasks.testing.junitplatform.JUnitPlatformOptions;
 import us.ihmc.continuousIntegration.bambooRestApi.BambooRestPlan;
 import us.ihmc.continuousIntegration.generator.BambooTestSuiteGenerator;
 import us.ihmc.continuousIntegration.model.AgileTestingStandaloneWorkspace;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TODO
@@ -36,12 +38,60 @@ public class IHMCContinuousIntegrationGradlePlugin implements Plugin<Project>
       projectPath = project.getProjectDir().toPath();
       multiProjectPath = project.getRootDir().toPath().resolve("..");
 
-      testSuitesConfiguration = new TestSuiteConfiguration(project.getProperties());
-      testSuitesConfiguration.hyphenatedName = project.getName();
-      testSuitesConfiguration.pascalCasedName = AgileTestingTools.hyphenatedToPascalCased(project.getName());
+      configureJUnitPlatform(project);
 
-      createTask(project, "generateTestSuites");
-      createTask(project, "generateTestSuitesMultiProject");
+      //      testSuitesConfiguration = new TestSuiteConfiguration(project.getProperties());
+//      testSuitesConfiguration.hyphenatedName = project.getName();
+//      testSuitesConfiguration.pascalCasedName = AgileTestingTools.hyphenatedToPascalCased(project.getName());
+//
+//      createTask(project, "generateTestSuites");
+//      createTask(project, "generateTestSuitesMultiProject");
+   }
+
+   private void configureJUnitPlatform(Project project)
+   {
+      try
+      {
+         Project testProject = project.project(project.getName() + "-test"); // assumes ihmc-build plugin
+         Test testExtension = (Test) testProject.getTasks().getByName("test");
+
+         testExtension.useJUnitPlatform(jUnitPlatformOptions -> {
+            project.getLogger().info("[ihmc-ci] Using JUnit platform");
+
+            includeTags(project, jUnitPlatformOptions, "includeTags");
+            includeTags(project, jUnitPlatformOptions, "includeTag");
+            excludeTags(project, jUnitPlatformOptions, "excludeTags");
+            excludeTags(project, jUnitPlatformOptions, "excludeTag");
+         });
+      }
+      catch (UnknownDomainObjectException e)
+      {
+         // do nothing
+      }
+   }
+
+   private void includeTags(Project project, JUnitPlatformOptions jUnitPlatformOptions, String propertyName)
+   {
+      if (project.hasProperty(propertyName) && !project.property(propertyName).equals("all"))
+      {
+         for (String includeTag : ((String) project.property(propertyName)).trim().toLowerCase().split(","))
+         {
+            project.getLogger().info("[ihmc-ci] Including tag: " + includeTag);
+            jUnitPlatformOptions.includeTags(includeTag);
+         }
+      }
+   }
+
+   private void excludeTags(Project project, JUnitPlatformOptions jUnitPlatformOptions, String propertyName)
+   {
+      if (project.hasProperty(propertyName) && !project.property(propertyName).equals("none"))
+      {
+         for (String excludeTag : ((String) project.property(propertyName)).trim().toLowerCase().split(","))
+         {
+            project.getLogger().info("[ihmc-ci] Excluding tag: " + excludeTag);
+            jUnitPlatformOptions.excludeTags(excludeTag);
+         }
+      }
    }
 
    @SuppressWarnings("unchecked")

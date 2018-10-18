76e3efd97f61e7eda866065fee97d0b9fbbb2755
Duncan Calvert <dcalvert@ihmc.us>
2018-10-18 12:21:17 -0500

    :bookmark: 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.20.6"
+   version = "1.0.1"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-ci"
    openSource = true
    maintainer = "Duncan Calvert"

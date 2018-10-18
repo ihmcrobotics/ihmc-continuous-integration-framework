349ea9475a5cb3767dddb2760a425fec97856a86
Duncan Calvert <dcalvert@ihmc.us>
2018-10-18 16:49:12 -0500

    :bookmark: 1.0.7, fix bug where runningOnCIServer was always true

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "1.0.6"
+   version = "1.0.7"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-ci"
    openSource = true
    maintainer = "Duncan Calvert"
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "us.ihmc"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
    mavenCentral()

fbb0e7da3b01c5327a8e83b4f4f7388211799e0d
Duncan Calvert <dcalvert@ihmc.us>
2018-10-23 15:03:04 -0500

    Minimal AssertionTools API.

--- AssertionTools.java
+++ AssertionTools.java
@@ -1,9 +1,5 @@
 package us.ihmc.robotics;
 
-import java.util.regex.Matcher;
-
-import org.junit.internal.ArrayComparisonFailure;
-
 public class AssertionTools
 {
    static public void assertTrue(String message, boolean condition)
@@ -41,22 +37,6 @@ public class AssertionTools
    {
    }
 
-   static public void assertNotEquals(String message, Object first, Object second)
-   {
-   }
-
-   static public void assertNotEquals(Object first, Object second)
-   {
-   }
-
-   private static void failEquals(String message, Object actual)
-   {
-   }
-
-   static public void assertNotEquals(String message, long first, long second)
-   {
-   }
-
    static public void assertNotEquals(long first, long second)
    {
    }
@@ -69,39 +49,7 @@ public class AssertionTools
    {
    }
 
-   public static void assertArrayEquals(String message, Object[] expecteds, Object[] actuals) throws ArrayComparisonFailure
-   {
-   }
-
    public static void assertArrayEquals(Object[] expecteds, Object[] actuals)
-   {
-   }
-
-   public static void assertArrayEquals(String message, byte[] expecteds, byte[] actuals) throws ArrayComparisonFailure
-   {
-   }
-
-   public static void assertArrayEquals(byte[] expecteds, byte[] actuals)
-   {
-   }
-
-   public static void assertArrayEquals(String message, char[] expecteds, char[] actuals) throws ArrayComparisonFailure
-   {
-   }
-
-   public static void assertArrayEquals(char[] expecteds, char[] actuals)
-   {
-   }
-
-   public static void assertArrayEquals(String message, short[] expecteds, short[] actuals) throws ArrayComparisonFailure
-   {
-   }
-
-   public static void assertArrayEquals(short[] expecteds, short[] actuals)
-   {
-   }
-
-   public static void assertArrayEquals(String message, int[] expecteds, int[] actuals) throws ArrayComparisonFailure
    {
    }
 
@@ -109,31 +57,11 @@ public class AssertionTools
    {
    }
 
-   public static void assertArrayEquals(String message, long[] expecteds, long[] actuals) throws ArrayComparisonFailure
-   {
-   }
-
-   public static void assertArrayEquals(long[] expecteds, long[] actuals)
-   {
-   }
-
-   public static void assertArrayEquals(String message, double[] expecteds, double[] actuals, double delta) throws ArrayComparisonFailure
-   {
-   }
-
    public static void assertArrayEquals(double[] expecteds, double[] actuals, double delta)
    {
    }
 
-   public static void assertArrayEquals(String message, float[] expecteds, float[] actuals, float delta) throws ArrayComparisonFailure
-   {
-   }
-
    public static void assertArrayEquals(float[] expecteds, float[] actuals, float delta)
-   {
-   }
-
-   private static void internalArrayEquals(String message, Object expecteds, Object actuals) throws ArrayComparisonFailure
    {
    }
 
@@ -150,16 +78,6 @@ public class AssertionTools
    }
 
    static public void assertEquals(String message, long expected, long actual)
-   {
-   }
-
-   @Deprecated
-   static public void assertEquals(double expected, double actual)
-   {
-   }
-
-   @Deprecated
-   static public void assertEquals(String message, double expected, double actual)
    {
    }
 
@@ -187,45 +105,11 @@ public class AssertionTools
    {
    }
 
-   static private void failNotNull(String message, Object actual)
-   {
-   }
-
-   static public void assertSame(String message, Object expected, Object actual)
-   {
-   }
-
    static public void assertSame(Object expected, Object actual)
    {
    }
 
-   static public void assertNotSame(String message, Object unexpected, Object actual)
-   {
-   }
-
    static public void assertNotSame(Object unexpected, Object actual)
-   {
-   }
-
-   static private void failSame(String message)
-   {
-   }
-
-   static private void failNotSame(String message, Object expected, Object actual)
-   {
-   }
-
-   static private void failNotEquals(String message, Object expected, Object actual)
-   {
-   }
-
-   @Deprecated
-   public static void assertEquals(String message, Object[] expecteds, Object[] actuals)
-   {
-   }
-
-   @Deprecated
-   public static void assertEquals(Object[] expecteds, Object[] actuals)
    {
    }
 }

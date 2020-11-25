c9ead8aa0a2137f892d581852cb3d45e09089cfa
Duncan Calvert <dcalvert@ihmc.us>
2020-11-24 23:33:47 -0600

    :arrow_up: fuel 2.3.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
    api("org.junit.platform:junit-platform-console:1.7.0")
    api("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-   api("com.github.kittinunf.fuel:fuel:2.0.1") // to upgrade, need kotlin 1.3.50+, i.e. Gradle 6+
+   api("com.github.kittinunf.fuel:fuel:2.3.0")
    api("org.json:json:20201115")
 }
 
